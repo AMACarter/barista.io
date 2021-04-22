78891471d148a7f69632ba6d1a706682e5e2e5ec
Adam Carter <67630275+AMACarter@users.noreply.github.com>
2021-04-22 21:23:45 +0100

    Add files via upload

---
+++ LevelDectectionSystem.ino
@@ -0,0 +1,39 @@
+///////////////////////////////////////////////
+////////////// SMART BARISTA //////////////////
+///////// LEVEL DETECTION SYSTEM //////////////
+///////////////////////////////////////////////
+
+int trigger = 1; //trigger pin
+int echo = 2; // pin that waits for echo
+int data; // calculated distance
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(trigger, OUTPUT);
+  pinMode(echo, INPUT); 
+}
+
+void loop()
+{
+  long t = 0; //time taken from transmission
+  
+  // Transmitting pulse
+  digitalWrite(trigger, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  
+  // Waiting for pulse
+  t = pulseIn(echo, HIGH);
+
+  data = (t/2) / 10.9; // Distance calculation
+
+  if (data <= 2)
+    {
+    // send signal to relay to turn off machine
+    }
+  
+  delay(1000);
+}
